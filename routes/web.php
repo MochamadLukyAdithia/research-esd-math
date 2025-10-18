a0a386380178aeab5bf98f4ec5d0e9cf3f5e4e8e
Richie Olajuwon Santoso <richieolajuwons@gmail.com>
2025-10-18 20:20:29 +0700

    feat: Add mapbox and portal page

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Middleware\RoleMiddleware;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Middleware\RoleMiddleware;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PortalForUserController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -31,10 +32,19 @@ Route::get('/tutorial', function () {
     return Inertia::render('Tutorial');
 })->name('tutorial');
 
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/portal', [PortalForUserController::class, 'index'])
+        ->name('portal.index');
+    Route::post('/portal/questions/{questionId}/toggle-favorite', [PortalForUserController::class, 'toggleFavorite'])
+        ->name('portal.questions.toggleFavorite');
+    Route::get('/portal/questions/{question}', [PortalForUserController::class, 'show'])
+        ->name('portal.questions.show');
 });
 
 require __DIR__ . '/auth.php';
