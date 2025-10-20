9d99c4d5e79d78e8d224b50210f291e61135c115
Stwn25 <msetiawanwibisono@gmail.com>
2025-10-20 19:27:53 +0700

    fix: unused line

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,7 +31,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UserSeeder::class,
-            DistrictSeeder::class,
             TagSeeder::class,
             QuestionSeeder::class,
             QuestionRelationSeeder::class,
